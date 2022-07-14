997b48005502f3412ef6c17d3d4f26dc41825ba6
Jaakko Lankila <japatius_96@hotmail.com>
2022-07-14 13:54:04 +0300

    move states and functions to product context

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useRef } from 'react';
 import {
   Box,
   SimpleGrid,
@@ -6,7 +6,6 @@ import {
   Text,
   Button,
   Icon,
-  useMediaQuery,
   Drawer,
   useDisclosure,
   DrawerOverlay,
@@ -15,13 +14,7 @@ import {
   DrawerHeader,
   DrawerCloseButton,
 } from '@chakra-ui/react';
-import {
-  RiArrowLeftSFill,
-  RiArrowLeftSLine,
-  RiArrowRightSFill,
-  RiArrowRightSLine,
-  RiFilterFill,
-} from 'react-icons/ri';
+import { RiArrowLeftSFill, RiArrowLeftSLine, RiArrowRightSFill, RiArrowRightSLine, RiFilterFill } from 'react-icons/ri';
 import { useQuery } from 'react-query';
 import { ProductContext } from '../../context/ProductContext';
 import { Product } from '../../interfaces/Product';
@@ -31,16 +24,12 @@ import ProductCard from './ProductCard';
 import { useCategories } from '../../hooks/query';
 import { queryClient } from '../../App';
 import { CategoryList } from '../Category/CategoryList';
-import { breakpoints } from '../../config/chakra';
 import { useAppMediaQuery } from '../../hooks';
 
 export const Products = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = useRef<HTMLButtonElement>(null);
   const productContext = useContext(ProductContext);
-  const [limit, setLimit] = useState<number>(9); // limit of products
-  const [skip, setSkip] = useState<number>(0); // how many products is wanted to be skipped
-  const [pages, setPages] = useState<number>(1); // how many pages of products
 
   const { largerThanMd } = useAppMediaQuery();
 
@@ -49,8 +38,9 @@ export const Products = () => {
 
   // use paginated strategy for fetching products from API
   const { isLoading, data, isPreviousData, error, status } = useQuery(
-    ['products', productContext?.category, skip],
-    () => fetchPaginatedProducts(productContext?.category, limit, skip),
+    ['products', productContext?.category, productContext?.paginationSkip],
+    () =>
+      fetchPaginatedProducts(productContext?.category, productContext?.productLimit, productContext?.paginationSkip),
     {
       initialData: () => {
         const initialProducts = queryClient.getQueryData<Product[]>(['products', '', 0]) ?? [];
@@ -62,6 +52,7 @@ export const Products = () => {
       keepPreviousData: true,
     }
   );
+  console.log(data);
 
   if (isLoading) {
     return <Loader />;
@@ -90,46 +81,36 @@ export const Products = () => {
             {status === 'success' &&
               data?.products.map((product: Product) => <ProductCard key={product.id} product={product} />)}
           </SimpleGrid>
-
           {/* Pagination */}
           <Flex maxW="container.lg" align="center" justify="center" marginTop="5" marginBottom="5">
             <Button
               mr="4"
-              onClick={() => {
-                setSkip(0);
-                setPages(1);
-              }}
-              disabled={skip === 0}
+              onClick={() => productContext?.firstPage()}
+              disabled={productContext?.paginationSkip === 0}
             >
               <Icon as={RiArrowLeftSFill} fontSize="2xl" />
             </Button>
             <Button
               mr="4"
-              onClick={() => {
-                setSkip((prev) => Math.max(prev - 9, 0));
-                setPages((prev) => prev - 1);
-              }}
-              disabled={skip === 0}
+              onClick={() => productContext?.prevPage()}
+              disabled={productContext?.paginationSkip === 0}
             >
               <Icon as={RiArrowLeftSLine} fontSize="2xl" />
             </Button>
-            <Text>Page {pages} of ?</Text>
+            <Text>Page {productContext?.pageNumber} of ?</Text>
             <Button
               ml="4"
               onClick={() => {
                 console.log(!isPreviousData);
-                if (!isPreviousData) {
-                  setSkip((prev) => prev + 9);
-                  setPages((prev) => prev + 1);
-                }
+                if (!isPreviousData) productContext?.nextPage();
               }}
-              disabled={data!.length <= 9}
+              disabled={data?.total <= 9}
             >
               <Icon as={RiArrowRightSLine} fontSize="2xl" />
             </Button>
-            <Button ml="4" disabled={data!.length <= 9}>
+            {/* <Button ml="4" disabled={data!.length <= 9}>
               <Icon as={RiArrowRightSFill} fontSize="2xl" />
-            </Button>
+            </Button> */}
           </Flex>
         </Box>
       </Flex>
--- src/context/ProductContext.tsx
+++ src/context/ProductContext.tsx
@@ -11,6 +11,12 @@ export enum FilterBy {
 
 type ContextProps = {
   category: string;
+  pageNumber: number;
+  productLimit: number;
+  paginationSkip: number;
+  firstPage: () => void;
+  prevPage: () => void;
+  nextPage: () => void;
   selectedProduct: string;
   setProduct: (name: string) => void;
   setCategory: (category: string) => void;
@@ -23,13 +29,45 @@ export const ProductContext = createContext<ContextProps | undefined>(undefined)
 
 function ProductProvider({ children }: ProviderProps) {
   const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [currentProductPage, setCurrentProductPage] = useState<number>(1);
+  const [productLimit, setProductLimit] = useState<number>(9);
+  const [skip, setSkip] = useState<number>(0);
   const [product, setProduct] = useState<string>('');
+
+  const handleSetCategory = (newCategory: string) =>
+    setSelectedCategory((prev) => {
+      // if new category is selected, set pages and skip to 0 and set new state
+      console.log(`selectedCategory ${selectedCategory}, prev ${prev}, newCategory ${newCategory}`)
+      if (selectedCategory === prev) {
+        setCurrentProductPage(0);
+        setSkip(0);
+        return newCategory;
+      }
+      return prev;
+    });
+
+  const goToFirstPage = () => {
+    setSkip(0);
+    setCurrentProductPage(1);
+  }
+
+  const goToPrevPage = () => {
+    setSkip((prev) => Math.max(prev - 9, 0));
+    setCurrentProductPage((prev) => prev - 1);
+  }
+
+  const goToNextPage = () => {
+    setSkip((prev) => prev + 9);
+    setCurrentProductPage((prev) => prev + 1);
+  }
+
+  // TODO
+  const goToLastPage = () => {}
 
   // TODO: if product is successfully searched, it should be appendended into searches state
   const [searches, setSearches] = useState<string[]>([]);
   const [filterRule, setFilterRule] = useState<FilterBy>(FilterBy.ORIGINAL);
 
-  const selectCategory = (category: string) => setSelectedCategory(category);
   const selectProduct = (name: string) => setProduct(name);
   // TODO
   const selectFilterRule = (rule: FilterBy) => setFilterRule(rule);
@@ -38,9 +76,15 @@ function ProductProvider({ children }: ProviderProps) {
     <ProductContext.Provider
       value={{
         category: selectedCategory,
+        pageNumber: currentProductPage,
+        productLimit: productLimit,
+        paginationSkip: skip,
+        firstPage: goToFirstPage,
+        nextPage: goToNextPage,
+        prevPage: goToPrevPage,
         setProduct: selectProduct,
         selectedProduct: product,
-        setCategory: selectCategory,
+        setCategory: handleSetCategory,
         searchHistory: searches,
         filterRule: filterRule,
         setFilterRule: selectFilterRule,
